e3373fb574d2a5e00ee94f3cf0fd39cb53fd43e6
Brady Phelps <bp309420@ohio.edu>
2023-04-02 23:01:28 -0400

    maybe

--- frontend/app/src/Projects.jsx
+++ frontend/app/src/Projects.jsx
@@ -35,12 +35,12 @@ function Projects() {
       </div>
       <div className= "visibledevice">
         <div className="mobile-construction">
-          <h1 className = "heading">Hi, I'm Brady</h1>
+          <h1>Currently under construction.  Try visiting my <a href="https://github.com/BP-2">GitHub</a> for some projects!</h1>
         </div>
       </div>
       <div className = "visibledesktop">
         <div className="construction">
-          <h1>Currently under construction.  Try visiting my <a href="https://github.com/BP-2">GitHub</a> for some projects! not mobile </h1>
+          <h1>Currently under construction.  Try visiting my <a href="https://github.com/BP-2">GitHub</a> for some projects! </h1>
         </div>
       </div>
       
