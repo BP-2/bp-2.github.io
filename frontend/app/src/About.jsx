5a18196dee0ee3c230c906711e122d01699a3488
Brady Phelps <bp309420@ohio.edu>
2023-04-02 23:13:43 -0400

    tetst

--- frontend/app/src/About.jsx
+++ frontend/app/src/About.jsx
@@ -40,7 +40,7 @@ function About() {
         <AboutMeCard />
       </div>
       <div className="visibledevice">
-        <div className="spacer-top"></div>
+        <div className="spacer-top"><p> </p></div>
       </div>
       <TimeLine />
     </div>
