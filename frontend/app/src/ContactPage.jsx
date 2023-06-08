db44d06f73d9bed54f9510af88008be33d8e6bd3
Brady Phelps <bp309420@ohio.edu>
2023-06-08 13:55:54 -0400

    Created form tied to firebase

--- frontend/app/src/ContactPage.jsx
+++ frontend/app/src/ContactPage.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
 import "./App.scss";
 import { Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
+import handleSubmit from "./handleSubmit";
+import { useRef } from "react";
+
+
+
+// GOOGLE FORM VARIANT: <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScsLIJMlIf9WwGzlGjz8sEQJnT6oysj8jVDz8XppkYhcjzjKA/viewform?embedded=true" width="640" height="812" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 
 function Contact() {
-  const [count, setCount] = useState(0);
+  const dataRefMessage = useRef();
+  const dataRefEmail = useRef();
+  const dataRefName = useRef();
 
+  const submithandler = (e) => {
+    e.preventDefault();
+    handleSubmit(dataRefMessage.current.value, dataRefName.current.value, dataRefEmail.current.value);
+    dataRefMessage.current.value = "";
+    dataRefName.current.value = "";
+    dataRefEmail.current.value = "";
+  }; 
   return (
     <div>
       <div className="visibledesktop">
@@ -33,9 +47,19 @@ function Contact() {
             Contact{" "}
           </Link>
         </div>
-
         <div className = "center-form">
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScsLIJMlIf9WwGzlGjz8sEQJnT6oysj8jVDz8XppkYhcjzjKA/viewform?embedded=true" width="640" height="812" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <form onSubmit={submithandler}>
+        <div className="center-items">
+          <p><b>Get in touch!</b></p><br/>
+          <p>Name: </p><input type="text" size="40" ref={dataRefName}></input>
+          <p>Email: </p> <input type="text" size = "40" ref = {dataRefEmail}></input>
+          <p>Message: </p>
+          <textarea rows="5" cols = "40" className="textarea" ref={dataRefMessage} />
+          <br />
+          <br />
+          <button className = "send-button" type="submit">Send</button>
+        </div>
+        </form> 
         </div>
       </div>
       <div className="visibledevice">
---
+++ frontend/app/src/handleSubmit.js
@@ -0,0 +1,20 @@
+import { addDoc, collection } from "@firebase/firestore"
+import { firestore } from "./firebase_setup/firebase.js"
+ 
+const handleSubmit = (message, name, email) => {
+    const ref = collection(firestore, "test_data") // Firebase creates this automatically
+ 
+    let data = {
+        userName: name,
+        userMail: email,
+        userMessage: message,
+    }
+    
+    try {
+        addDoc(ref, data)
+    } catch(err) {
+        console.log(err)
+    }
+}
+ 
+export default handleSubmit
