60e7b306af8a63198432131dac84d3a76f8ed37c
Brady Phelps <bp309420@ohio.edu>
2024-05-05 01:52:19 -0400

    Fixed the form

--- frontend/app/src/ContactPage.jsx
+++ frontend/app/src/ContactPage.jsx
@@ -10,6 +10,11 @@ import emailjs from "@emailjs/browser";
 
 function Contact() {
   const form = useRef();
+  const formTwo = useRef();
+  const dataRefMessage = useRef();
+  const dataRefEmail = useRef();
+  const dataRefName = useRef();
+
   const submithandler = (e) => {
     e.preventDefault();
       try{
@@ -33,8 +38,39 @@ function Contact() {
       alert("An issue has occurred with emailJS. Please reach me directly at bradyDummy@gmail.com.");
       }
   };
+  const submithandlerMobile = (e) => {
+    e.preventDefault();
+      try{
+      emailjs
+        .sendForm(
+          "service_97c8c66",
+          "template_42ed68m",
+          formTwo.current, {
+          publicKey:"O_5oHo3Aff8RX-TCf"})
+        .then(
+          (result) => {
+            console.log("SUCCESS");
+          },
+          (error) => {
+            console.log("FAIL");
+          }
+        );
+        alert("Thanks for reaching out!  Your response has been recorded. :)");
+      }
+      catch{
+      alert("An issue has occurred with emailJS. Please reach me directly at bradyDummy@gmail.com.");
+      }
+  };
 
-
+  const setName = (e) => {
+    dataRefName.current.value = e.target.value;
+  };
+  const setMail = (e) => {
+    dataRefEmail.current.value = e.target.value;
+  };
+  const setMessage = (e) => {
+    dataRefMessage.current.value = e.target.value;
+  };
 
   return (
     <div>
@@ -127,24 +163,26 @@ function Contact() {
           </Link>
         </div>
         <div className="center-form-mobile">
-          <form ref={form} onSubmit={submithandler}>
+          <form ref={formTwo} onSubmit={submithandlerMobile}>
             <div className="center-items">
               <p>
                 <b>Get in touch!</b>
               </p>
               <br />
               <p>Name: </p>
-              <input type="text" size="25" name="user_name"></input>
+              <input type="text" size="25" name="user_name"
+                onChange={(e) => setName(e)}></input>
               <p>Email: </p>{" "}
-              <input size="25" 
-                type="email" 
-                name="user_email"></input>
+              <input type="email" size="25" 
+                name="user_email"
+                onChange={(e) => setMail(e)}></input>
               <p>Message: </p>
               <textarea
                 rows="5"
                 cols="25"
                 className="textarea"
                 name="message"
+                onChange={(e) => setMessage(e)}
               />
               <br />
               <br />
